5ae364ead2d064ef608bf941428c72565ecdf978
Fumika Matsuda <fumika.matsuda@tuni.fi>
2024-11-12 18:12:07 +0200

    fix some codes related to docker fail

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,43 +1,43 @@
 "use client";
-import Link from "next/link";
-import { useForm } from "react-hook-form";
-import { UserCredentials, validateUser } from "../actions/login";
-import { useEffect, useState } from "react";
+// import Link from "next/link";
+// import { useForm } from "react-hook-form";
+// import { UserCredentials, validateUser } from "../actions/login";
+// import { useEffect, useState } from "react";
 
 export default function Quiz() {
 
-	return (
-        <div className="flex w-full bg-gray-100 p-3" style={{height: "88dvh"}}>
+    return (
+        <div className="flex w-full bg-gray-100 p-3" style={{ height: "88dvh" }}>
             <div className="basis-1/4 me-3 shadow-lg bg-white p-3 h-full rounded overflow-auto">
-            
+
                 <h3 className="font-bold">This week</h3>
 
                 <div className="flex w-100 h-20 rounded-xl mt-3 p-3 bg-gray-200 shadow-lg">
                     <div className="basis-3/4">
                         <p>Dark patterns</p>
                     </div>
-                        <div className="basis-1/4 flex flex-col justify-between">
-                            <p className="bg-blue-500 text-center text-white rounded-xl">
-                                4/5
-                            </p>
-                            <p className="text-sm text-gray-500">
-                                15/10/2024
-                            </p>
-                        </div>
+                    <div className="basis-1/4 flex flex-col justify-between">
+                        <p className="bg-blue-500 text-center text-white rounded-xl">
+                            4/5
+                        </p>
+                        <p className="text-sm text-gray-500">
+                            15/10/2024
+                        </p>
+                    </div>
                 </div>
 
                 <div className="flex w-100 h-20 rounded-xl mt-3 p-3 bg-white shadow-lg">
                     <div className="basis-3/4">
                         <p>Dark patterns</p>
                     </div>
-                        <div className="basis-1/4 flex flex-col justify-between">
-                            <p className="bg-blue-500 text-center text-white rounded-xl">
-                                4/5
-                            </p>
-                            <p className="text-sm text-gray-500">
-                                15/10/2024
-                            </p>
-                        </div>
+                    <div className="basis-1/4 flex flex-col justify-between">
+                        <p className="bg-blue-500 text-center text-white rounded-xl">
+                            4/5
+                        </p>
+                        <p className="text-sm text-gray-500">
+                            15/10/2024
+                        </p>
+                    </div>
                 </div>
 
                 <h3 className="font-bold mt-3">This month</h3>
@@ -46,28 +46,28 @@ export default function Quiz() {
                     <div className="basis-3/4">
                         <p>Dark patterns</p>
                     </div>
-                        <div className="basis-1/4 flex flex-col justify-between">
-                            <p className="bg-blue-500 text-center text-white rounded-xl">
-                                4/5
-                            </p>
-                            <p className="text-sm text-gray-500">
-                                15/10/2024
-                            </p>
-                        </div>
+                    <div className="basis-1/4 flex flex-col justify-between">
+                        <p className="bg-blue-500 text-center text-white rounded-xl">
+                            4/5
+                        </p>
+                        <p className="text-sm text-gray-500">
+                            15/10/2024
+                        </p>
+                    </div>
                 </div>
 
                 <div className="flex w-100 h-20 rounded-xl mt-3 p-3 bg-white shadow-lg">
                     <div className="basis-3/4">
                         <p>Dark patterns</p>
                     </div>
-                        <div className="basis-1/4 flex flex-col justify-between">
-                            <p className="bg-blue-500 text-center text-white rounded-xl">
-                                4/5
-                            </p>
-                            <p className="text-sm text-gray-500">
-                                15/10/2024
-                            </p>
-                        </div>
+                    <div className="basis-1/4 flex flex-col justify-between">
+                        <p className="bg-blue-500 text-center text-white rounded-xl">
+                            4/5
+                        </p>
+                        <p className="text-sm text-gray-500">
+                            15/10/2024
+                        </p>
+                    </div>
                 </div>
                 <h3 className="font-bold">Previous</h3>
 
@@ -75,34 +75,34 @@ export default function Quiz() {
                     <div className="basis-3/4">
                         <p>Dark patterns</p>
                     </div>
-                        <div className="basis-1/4 flex flex-col justify-between">
-                            <p className="bg-blue-500 text-center text-white rounded-xl">
-                                4/5
-                            </p>
-                            <p className="text-sm text-gray-500">
-                                15/10/2024
-                            </p>
-                        </div>
+                    <div className="basis-1/4 flex flex-col justify-between">
+                        <p className="bg-blue-500 text-center text-white rounded-xl">
+                            4/5
+                        </p>
+                        <p className="text-sm text-gray-500">
+                            15/10/2024
+                        </p>
+                    </div>
                 </div>
 
                 <div className="flex w-100 h-20 rounded-xl mt-3 p-3 bg-white shadow-lg">
                     <div className="basis-3/4">
                         <p>Dark patterns</p>
                     </div>
-                        <div className="basis-1/4 flex flex-col justify-between">
-                            <p className="bg-blue-500 text-center text-white rounded-xl">
-                                4/5
-                            </p>
-                            <p className="text-sm text-gray-500">
-                                15/10/2024
-                            </p>
-                        </div>
+                    <div className="basis-1/4 flex flex-col justify-between">
+                        <p className="bg-blue-500 text-center text-white rounded-xl">
+                            4/5
+                        </p>
+                        <p className="text-sm text-gray-500">
+                            15/10/2024
+                        </p>
+                    </div>
                 </div>
             </div>
 
 
             <div className="basis-3/4 flex flex-col shadow-lg p-3 h-full rounded">
-                <div className="basis-2/12 flex flex-row justify-between" style={{height: "10%"}}>
+                <div className="basis-2/12 flex flex-row justify-between" style={{ height: "10%" }}>
                     <div className="flex flex-col justify-center">
                         <p>15/10/2024</p>
                         <p>15:34</p>
@@ -113,11 +113,11 @@ export default function Quiz() {
                     <div className="flex items-center">
                         <p>Score: <span className="bg-blue-500 text-white rounded-md px-2 py-1">4/5</span></p>
                     </div>
-                    
+
 
                 </div>
-                <div className="basis-10/12" style={{height: "90%"}}>
-                
+                <div className="basis-10/12" style={{ height: "90%" }}>
+
 
                     <div className="w-full h-full p-4 rounded-xl bg-gray-200 shadow-lg">
                         <div className="w-full h-full rounded-xl border p-4 border-gray-400 bg-white shadow-lg">
@@ -143,7 +143,7 @@ export default function Quiz() {
                                 </div>
 
                                 <hr className="h-px my-8 bg-gray-200 border-0"></hr>
-                                
+
                                 <div className="flex justify-between mb-2">
                                     <p className="text-gray-500">Question 2 topic</p>
                                     <p className="text-gray-500">0/1</p>
@@ -151,15 +151,15 @@ export default function Quiz() {
                                 </div>
                                 <p className="mb-2"><strong>2. Question number 2?</strong></p>
                                 <div className="flex items-center mb-2">
-                                    <input disabled type="radio" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"/>
+                                    <input disabled type="radio" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2" />
                                     <label className="ms-2 text-gray-900">Option 1</label>
                                 </div>
                                 <div className="flex items-center mb-2">
-                                    <input disabled type="radio" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"/>
+                                    <input disabled type="radio" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2" />
                                     <label className="ms-2 text-gray-900">Option 2 <span className="ms-3 text-gray-400">Correct answer</span></label>
                                 </div>
                                 <div className="flex items-center mb-2">
-                                    <input checked type="radio" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"/>
+                                    <input checked type="radio" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2" />
                                     <label className="ms-2 text-gray-900">Option 3</label>
                                 </div>
                                 <div className="border border-gray-300 p-2 rounded-xl ">
@@ -174,5 +174,5 @@ export default function Quiz() {
             </div>
 
         </div>
-	);
+    );
 }
