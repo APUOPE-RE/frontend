be74a7887198e80cc46d27cb752af18d974f0146
Fumika Matsuda <fumika.matsuda@tuni.fi>
2024-11-20 21:21:04 +0200

    add correct/incorrect icons

--- src/app/previous-quizzes/page.tsx
+++ src/app/previous-quizzes/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { UserCredentials, validateUser } from "../actions/login";
@@ -6,100 +7,99 @@ import { useEffect, useState } from "react";
 
 const QUIZ_LIST = [
     {
-      id: 1,
-      title: "Dark patterns",
-      date: "15/10/2024",
-      time: "15:34",
-      score: {
-        current: 4,
-        total: 5
-      },
-      questions: [
-        {
-          id: 1,
-          topic: "Question 1 topic",
-          text: "Question number 1?",
-          type: "open",
-          answer: "This is a test answer for open question number 1.",
-          feedback: "This is some feedback from the AI.",
-          correct: false,
-          value: 1,
-          score: 0
+        id: 1,
+        title: "Dark patterns",
+        date: "15/10/2024",
+        time: "15:34",
+        score: {
+            current: 4,
+            total: 5
         },
-        {
-          id: 2,
-          topic: "Question 2 topic",
-          text: "Question number 2?",
-          type: "multiple_choice",
-          options: [
-            { text: "Option 1", is_correct: false },
-            { text: "Option 2", is_correct: true, is_selected: true },
-            { text: "Option 3", is_correct: false }
-          ],
-          feedback: "This is some feedback from the AI.",
-          correct: false,
-          value: 1,
-          score: 1
-        }
-      ]
+        questions: [
+            {
+                id: 1,
+                topic: "Question 1 topic",
+                text: "Question number 1?",
+                type: "open",
+                answer: "This is a test answer for open question number 1.",
+                feedback: "This is some feedback from the AI.",
+                correct: false,
+                value: 1,
+                score: 0
+            },
+            {
+                id: 2,
+                topic: "Question 2 topic",
+                text: "Question number 2?",
+                type: "multiple_choice",
+                options: [
+                    { text: "Option 1", is_correct: false },
+                    { text: "Option 2", is_correct: true, is_selected: true },
+                    { text: "Option 3", is_correct: false }
+                ],
+                feedback: "This is some feedback from the AI.",
+                correct: false,
+                value: 1,
+                score: 1
+            }
+        ]
     },
     {
-      id: 2,
-      title: "User Experience",
-      date: "18/10/2024",
-      time: "10:20",
-      score: {
-        current: 3,
-        total: 5
-      },
-      questions: [
-        {
-          id: 1,
-          topic: "Question 1 topic",
-          text: "What is User Experience?",
-          type: "open",
-          answer: "A broad term related to the end-user's interaction with a product.",
-          feedback: "Great explanation!",
-          correct: true,
-          value: 1,
-          score: 1
+        id: 2,
+        title: "User Experience",
+        date: "18/10/2024",
+        time: "10:20",
+        score: {
+            current: 3,
+            total: 5
         },
-        {
-          id: 2,
-          topic: "Question 2 topic",
-          text: "What affects UX?",
-          type: "multiple_choice",
-          options: [
-            { text: "Company's income", is_correct: false },
-            { text: "Aesthetics", is_correct: true },
-            { text: "Company's internal process", is_correct: false, is_selected: true }
-          ],
-          feedback: "Consider the user's perspective.",
-          correct: false,
-          value: 1,
-          score: 0
-        }
-      ]
+        questions: [
+            {
+                id: 1,
+                topic: "Question 1 topic",
+                text: "What is User Experience?",
+                type: "open",
+                answer: "A broad term related to the end-user's interaction with a product.",
+                feedback: "Great explanation!",
+                correct: true,
+                value: 1,
+                score: 1
+            },
+            {
+                id: 2,
+                topic: "Question 2 topic",
+                text: "What affects UX?",
+                type: "multiple_choice",
+                options: [
+                    { text: "Company's income", is_correct: false },
+                    { text: "Aesthetics", is_correct: true },
+                    { text: "Company's internal process", is_correct: false, is_selected: true }
+                ],
+                feedback: "Consider the user's perspective.",
+                correct: false,
+                value: 1,
+                score: 0
+            }
+        ]
     }
-  ];
-  
-  
+];
+
+
 
 export default function Quiz() {
     const [selectedQuizId, setSelectedQuizId] = useState(QUIZ_LIST[0].id);
 
     const selectedQuiz = QUIZ_LIST.find(quiz => quiz.id === selectedQuizId);
 
-	return (
-        <div className="flex w-full bg-gray-100 p-3" style={{height: "88dvh"}}>
+    return (
+        <div className="flex w-full bg-gray-100 p-3" style={{ height: "88dvh" }}>
             <div className="basis-1/4 me-3  bg-white p-3 h-full rounded overflow-auto">
-            
+
                 {QUIZ_LIST.map(quiz => (
                     <div
                         key={quiz.id}
-                        className={`flex w-100 h-20 rounded-xl mt-3 p-3 cursor-pointer hover:bg-gray-200 shadow-lg ${
-                            selectedQuizId === quiz.id ? 'border border-gray-400' : 'bg-white'
-                          }`}
+                        className={`flex w-100 h-20 rounded-xl mt-3 p-3 cursor-pointer hover:bg-gray-200 shadow-lg ${selectedQuizId === quiz.id ? 'border border-gray-400' : 'bg-white'
+                            }`}
                         onClick={() => setSelectedQuizId(quiz.id)}
                     >
                         <div className="basis-3/4">
@@ -144,45 +144,50 @@ export default function Quiz() {
                                                     <p className="text-gray-500">{question.topic}</p>
                                                     <p className="text-gray-500">{question.score}/{question.value}</p>
                                                 </div>
-                                                
+
                                                 <p className="mb-2"><strong>{index + 1}. {question.text}</strong></p>
 
                                                 {question.type === 'open' ? (
-                                                    <div 
-                                                    className={`flex border p-2 mb-2 rounded-xl ${
-                                                        question.correct ? "border-lime-500" : "border-red-500"
-                                                      }`}
+                                                    <div
+                                                        className={`flex border p-2 mb-2 rounded-xl ${question.correct ? "border-lime-500" : "border-red-500"
+                                                            }`}
                                                     >
                                                         <div className="basis-11/12">
                                                             <strong>Answer:</strong>
                                                             <p>{question.answer}</p>
                                                         </div>
-                                                        <div className="basis-1/12">
-                                                            <p className={`${question.correct ? "text-lime-500" : "text-red-500"}`}>
-                                                                {question.correct ? "Correct!" : "Incorrect!"}
-                                                            </p>
+                                                        <div className="basis-1/12 flex items-center justify-center">
+                                                            <Image
+                                                                src={question.correct ? "/correct.png" : "/incorrect.png"}
+                                                                alt={question.correct ? "Correct" : "Incorrect"}
+                                                                width={40}
+                                                                height={40}
+                                                            />
                                                         </div>
                                                     </div>
                                                 ) : (
                                                     question.options.map((option, optionIndex) => (
                                                         <div key={optionIndex} className={`flex items-center mb-2 p-2 ${option.is_selected && "border rounded-xl"} ${option.is_selected && option.is_correct ? "border-lime-500" : "border-red-500"}`}>
                                                             <div className="basis-11/12">
-                                                                <input 
-                                                                    type="radio" 
-                                                                    disabled 
-                                                                    checked={option.is_selected || false} 
-                                                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2" 
+                                                                <input
+                                                                    type="radio"
+                                                                    disabled
+                                                                    checked={option.is_selected || false}
+                                                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 focus:ring-2"
                                                                 />
                                                                 <label className="ms-2 text-gray-900">
-                                                                    {option.text} 
+                                                                    {option.text}
                                                                     {option.is_correct && <span className="ms-3 text-gray-400">Correct answer</span>}
                                                                 </label>
                                                             </div>
                                                             {option.is_selected && (
-                                                                <div className="basis-1/12">
-                                                                    <p className={`${option.is_correct ? "text-lime-500" : "text-red-500"}`}>
-                                                                        {option.is_correct ? "Correct!" : "Incorrect!"}
-                                                                    </p>
+                                                                <div className="basis-1/12 flex items-center justify-center">
+                                                                    <Image
+                                                                        src={option.is_correct ? "/correct.png" : "/incorrect.png"}
+                                                                        alt={option.is_correct ? "Correct" : "Incorrect"}
+                                                                        width={40}
+                                                                        height={40}
+                                                                    />
                                                                 </div>
                                                             )}
                                                         </div>
@@ -207,5 +212,5 @@ export default function Quiz() {
                 )}
             </div>
         </div>
-	);
+    );
 }
