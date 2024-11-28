a761e21f9ff9c44aa309f4b2743671a8d61ffb5c
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2024-11-28 21:07:06 +0200

    Reverted login page title change

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,6 @@ import { UserCredentials } from "../types/types";
 import { useSearchParams, useRouter } from "next/navigation";
 import { verifyAccount } from "../actions/verification";
 import { useAppContext } from "@/src/context";
-import Head from "next/head";
 
 function Login() {
 	const router = useRouter();
@@ -71,81 +70,76 @@ function Login() {
 	};
 
 	return (
-		<>
-		<head>
-			<title>Login - APUOPE-RE</title>
-		</head>
-			<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-				{token !== null && accountVerified && (
-					<div className="flex text-m p-5 max-w-md w-full justify-center mb-8 rounded-lg bg-green-200">
-						Account verified!
-					</div>
-				)}
-				{registerSuccess !== "" && (
-					<div className="flex text-m p-5 max-w-md w-full justify-center mb-8 rounded-lg bg-green-200">
-						{registerSuccess}
-					</div>
-				)}
-				<div className="w-full max-w-md p-8 pt-16 bg-white rounded-lg shadow-md">
-					<h2 className="text-2xl font-bold mb-3 text-center mb-10">
-						Login
-					</h2>
-					<form onSubmit={handleSubmit(handleLogin)}>
-						<div className="mb-4">
-							<label
-								className="block text-gray-700 text-sm font-bold mb-2"
-								htmlFor="email"
-							>
-								Email
-							</label>
-							<input
-								id="email"
-								type="email"
-								placeholder="Enter your email"
-								className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-								{...register("email")}
-								required
-							/>
-						</div>
-						<div className="mb-6">
-							<label
-								className="block text-gray-700 text-sm font-bold mb-2"
-								htmlFor="password"
-							>
-								Password
-							</label>
-							<input
-								id="passwordHash"
-								type="password"
-								placeholder="Enter your password"
-								className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-								{...register("passwordHash")}
-								required
-							/>
-						</div>
-						<div className="flex w-auto pb-2 text-sm text-gray-600 hover:text-blue-400">
-							<Link href="/forgot-password">
-								Forgot your password?
-							</Link>
-						</div>
-						<div className="flex items-center justify-between">
-							<p>
-								{"Don't have an account "}
-								<span className=" text-blue-800 font-semibold underline pl-1">
-									<Link href="/register">Register</Link>
-								</span>
-							</p>
-							<input
-								id="submit-login"
-								type="submit"
-								className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring"
-								value={"Login"}
-							/>
-						</div>
-					</form>
+		<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+			{token !== null && accountVerified && (
+				<div className="flex text-m p-5 max-w-md w-full justify-center mb-8 rounded-lg bg-green-200">
+					Account verified!
 				</div>
+			)}
+			{registerSuccess !== "" && (
+				<div className="flex text-m p-5 max-w-md w-full justify-center mb-8 rounded-lg bg-green-200">
+					{registerSuccess}
+				</div>
+			)}
+			<div className="w-full max-w-md p-8 pt-16 bg-white rounded-lg shadow-md">
+				<h2 className="text-2xl font-bold mb-3 text-center mb-10">
+					Login
+				</h2>
+				<form onSubmit={handleSubmit(handleLogin)}>
+					<div className="mb-4">
+						<label
+							className="block text-gray-700 text-sm font-bold mb-2"
+							htmlFor="email"
+						>
+							Email
+						</label>
+						<input
+							id="email"
+							type="email"
+							placeholder="Enter your email"
+							className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+							{...register("email")}
+							required
+						/>
+					</div>
+					<div className="mb-6">
+						<label
+							className="block text-gray-700 text-sm font-bold mb-2"
+							htmlFor="password"
+						>
+							Password
+						</label>
+						<input
+							id="passwordHash"
+							type="password"
+							placeholder="Enter your password"
+							className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+							{...register("passwordHash")}
+							required
+						/>
+					</div>
+					<div className="flex w-auto pb-2 text-sm text-gray-600 hover:text-blue-400">
+						<Link href="/forgot-password">
+							Forgot your password?
+						</Link>
+					</div>
+					<div className="flex items-center justify-between">
+						<p>
+							{"Don't have an account "}
+							<span className=" text-blue-800 font-semibold underline pl-1">
+								<Link href="/register">Register</Link>
+							</span>
+						</p>
+						<input
+							id="submit-login"
+							type="submit"
+							className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring"
+							value={"Login"}
+						/>
+					</div>
+				</form>
 			</div>
-		</>
+		</div>
 	);
 }
 
