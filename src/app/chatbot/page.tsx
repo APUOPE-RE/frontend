3c6e9cc2e6cd987c298cc59e84c6014a760995cb
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2024-11-28 20:07:32 +0200

    Added title to Chatbot page

--- src/app/chatbot/page.tsx
+++ src/app/chatbot/page.tsx
@@ -11,6 +11,7 @@ import {
 } from "../types/types";
 import { IoSendSharp } from "react-icons/io5";
 import { Conversations } from "./Conversations";
+import Head from "next/head";
 
 export default function Chatbot() {
 	const [messages, setMessages] = useState<Message[]>([]);
@@ -73,162 +74,167 @@ export default function Chatbot() {
 	};
 
 	return (
-		<div
-			className="flex flex-row bg-gray-100 py-3"
-			style={{ height: "88dvh" }}
-		>
-			<div className="basis-1/4 bg-white p-3 h-full mx-3 rounded">
-				<div className="flex justify-between items-center w-full">
-					<h1 className="font-extrabold text-[4vw]">Chatbot</h1>
-					<Image
-						src="/new_conversation.png"
-						alt="Icon for creating new conversation"
-						width={36}
-						height={36}
-						className="cursor-pointer object-contain"
-						onClick={() => setIsModelOpen(true)}
-					/>
+		<>
+		<head>
+			<title>Chatbot - APUOPE-RE</title>
+		</head>
+			<div
+				className="flex flex-row bg-gray-100 py-3"
+				style={{ height: "88dvh" }}
+			>
+				<div className="basis-1/4 bg-white p-3 h-full mx-3 rounded">
+					<div className="flex justify-between items-center w-full">
+						<h1 className="font-extrabold text-[4vw]">Chatbot</h1>
+						<Image
+							src="/new_conversation.png"
+							alt="Icon for creating new conversation"
+							width={36}
+							height={36}
+							className="cursor-pointer object-contain"
+							onClick={() => setIsModelOpen(true)}
+						/>
+					</div>
+					<div className="overflow-auto" style={{ height: "90%" }}>
+						<Conversations
+							currentConversation={conversationId}
+							setMessages={setMessages}
+							setValue={setValue}
+						/>
+					</div>
 				</div>
-				<div className="overflow-auto" style={{ height: "90%" }}>
-					<Conversations
-						currentConversation={conversationId}
-						setMessages={setMessages}
-						setValue={setValue}
-					/>
-				</div>
-			</div>
 
-			<div className="basis-3/4 bg-white p-3 me-3 h-full rounded">
-				<div className="w-100" style={{ height: "90%" }}>
-					<div className="h-full w-full p-4 border bg-gray-100 border-gray-300 rounded-lg space-y-4 overflow-auto">
-						{messages.map((msg, idx) => (
-							<div key={idx}>
-								{msg.from === "user" && (
-									<div className="flex justify-end">
-										<div className="w-1/2 border p-2 bg-white border-gray-300 rounded-lg">
+				<div className="basis-3/4 bg-white p-3 me-3 h-full rounded">
+					<div className="w-100" style={{ height: "90%" }}>
+						<div className="h-full w-full p-4 border bg-gray-100 border-gray-300 rounded-lg space-y-4 overflow-auto">
+							{messages.map((msg, idx) => (
+								<div key={idx}>
+									{msg.from === "user" && (
+										<div className="flex justify-end">
+											<div className="w-1/2 border p-2 bg-white border-gray-300 rounded-lg">
+												{msg.message}
+											</div>
+										</div>
+									)}
+									{msg.from === "bot" && (
+										<div className="w-1/2 border text-white p-2 bg-blue-500 border-gray-300 rounded-lg">
 											{msg.message}
 										</div>
-									</div>
-								)}
-								{msg.from === "bot" && (
-									<div className="w-1/2 border text-white p-2 bg-blue-500 border-gray-300 rounded-lg">
-										{msg.message}
-									</div>
-								)}
-							</div>
-						))}
-					</div>
-				</div>
-
-				<div
-					className="flex w-full items-center"
-					style={{ height: "10dvh" }}
-				>
-					<form
-						className="w-full"
-						onSubmit={handleSubmit(handleRequest)}
-					>
-						<div className="flex">
-							<input
-								type="text"
-								id="input"
-								className="basis-11/12 me-2 p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
-								placeholder="Say something..."
-								{...register("content", { required: true })}
-								disabled={disableInputField}
-							/>
-							<button
-								type="submit"
-								className="text-white bg-blue-700 font-medium rounded-lg text-sm px-4 py-2 basis-1/12 flex justify-center items-center"
-							>
-								<IoSendSharp className="text-2xl" />
-							</button>
-						</div>
-					</form>
-				</div>
-			</div>
-			{isModelOpen && (
-				<div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-					<div className="bg-white flex flex-col justify-center items-center p-6 rounded-lg shadow-lg w-[40%] h-[70%] text-center">
-						<div>
-							<h2 className="text-2xl font-bold mb-2">
-								New Conversation
-							</h2>
-							<p className="mb-6">
-								Select a topic for your next conversation:
-							</p>
-						</div>
-
-						<div className="flex flex-col items-center border rounded-lg w-[80%] h-[70%] overflow-auto">
-							<form
-								className="w-full relative"
-								style={{
-									position: "sticky",
-									top: 0,
-									zIndex: 10,
-									backdropFilter: "blur(10px)",
-								}}
-							>
-								<div>
-									<input
-										type="search"
-										placeholder="Search topic"
-										value={topic}
-										onChange={(e) =>
-											setTopic(e.target.value)
-										}
-										className="w-full bg-slate-100 p-2 rounded-md outline-none"
-									/>
-								</div>
-							</form>
-							{filteredItems.map((item) => (
-								<div
-									className="flex justify-between items-center my-1 px-4 w-[98%] h-20 rounded-xl py-2 bg-gray-100 shadow-md cursor-pointer hover:bg-blue-100"
-									key={item.id}
-									onClick={() => setSelectedTopic(item.id)}
-								>
-									<label
-										htmlFor={item.label}
-										className="cursor-pointer"
-									>
-										{item.label}
-									</label>
-									<input
-										id={item.label}
-										type="radio"
-										value={item.id}
-										checked={selectedTopic === item.id}
-										readOnly
-									/>
+									)}
 								</div>
 							))}
-							<div></div>
-						</div>
-						<div className="w-full flex justify-end pt-4 gap-4 px-4">
-							<button
-								className="bg-rose-500 text-white py-2 px-4 rounded"
-								onClick={() => (
-									setSelectedTopic(0), setIsModelOpen(false)
-								)}
-							>
-								Close
-							</button>
-							<button
-								className="bg-blue-500 text-white py-2 px-4 rounded"
-								onClick={() => (
-									reset(),
-									setValue("lectureId", selectedTopic),
-									setIsModelOpen(false),
-									setSelectedTopic(0),
-									setMessages([])
-								)}
-							>
-								Create
-							</button>
 						</div>
 					</div>
+
+					<div
+						className="flex w-full items-center"
+						style={{ height: "10dvh" }}
+					>
+						<form
+							className="w-full"
+							onSubmit={handleSubmit(handleRequest)}
+						>
+							<div className="flex">
+								<input
+									type="text"
+									id="input"
+									className="basis-11/12 me-2 p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+									placeholder="Say something..."
+									{...register("content", { required: true })}
+									disabled={disableInputField}
+								/>
+								<button
+									type="submit"
+									className="text-white bg-blue-700 font-medium rounded-lg text-sm px-4 py-2 basis-1/12 flex justify-center items-center"
+								>
+									<IoSendSharp className="text-2xl" />
+								</button>
+							</div>
+						</form>
+					</div>
 				</div>
-			)}
-		</div>
+				{isModelOpen && (
+					<div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+						<div className="bg-white flex flex-col justify-center items-center p-6 rounded-lg shadow-lg w-[40%] h-[70%] text-center">
+							<div>
+								<h2 className="text-2xl font-bold mb-2">
+									New Conversation
+								</h2>
+								<p className="mb-6">
+									Select a topic for your next conversation:
+								</p>
+							</div>
+
+							<div className="flex flex-col items-center border rounded-lg w-[80%] h-[70%] overflow-auto">
+								<form
+									className="w-full relative"
+									style={{
+										position: "sticky",
+										top: 0,
+										zIndex: 10,
+										backdropFilter: "blur(10px)",
+									}}
+								>
+									<div>
+										<input
+											type="search"
+											placeholder="Search topic"
+											value={topic}
+											onChange={(e) =>
+												setTopic(e.target.value)
+											}
+											className="w-full bg-slate-100 p-2 rounded-md outline-none"
+										/>
+									</div>
+								</form>
+								{filteredItems.map((item) => (
+									<div
+										className="flex justify-between items-center my-1 px-4 w-[98%] h-20 rounded-xl py-2 bg-gray-100 shadow-md cursor-pointer hover:bg-blue-100"
+										key={item.id}
+										onClick={() => setSelectedTopic(item.id)}
+									>
+										<label
+											htmlFor={item.label}
+											className="cursor-pointer"
+										>
+											{item.label}
+										</label>
+										<input
+											id={item.label}
+											type="radio"
+											value={item.id}
+											checked={selectedTopic === item.id}
+											readOnly
+										/>
+									</div>
+								))}
+								<div></div>
+							</div>
+							<div className="w-full flex justify-end pt-4 gap-4 px-4">
+								<button
+									className="bg-rose-500 text-white py-2 px-4 rounded"
+									onClick={() => (
+										setSelectedTopic(0), setIsModelOpen(false)
+									)}
+								>
+									Close
+								</button>
+								<button
+									className="bg-blue-500 text-white py-2 px-4 rounded"
+									onClick={() => (
+										reset(),
+										setValue("lectureId", selectedTopic),
+										setIsModelOpen(false),
+										setSelectedTopic(0),
+										setMessages([])
+									)}
+								>
+									Create
+								</button>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
 	);
 }
