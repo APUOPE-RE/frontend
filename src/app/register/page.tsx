cf0f0e174d9158530a21e4581307a7a1962ecf3d
Atte Tanskanen <94180117+tan-at@users.noreply.github.com>
2024-11-28 20:06:28 +0200

    Added title to Register page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -6,6 +6,7 @@ import { registerUser } from "../actions/registration";
 import { RegistrationData } from "../types/types";
 import { useRouter } from "next/navigation";
 import { useAppContext } from "@/src/context/index";
+import Head from "next/head";
 
 export default function Register() {
 	const router = useRouter();
@@ -66,111 +67,116 @@ export default function Register() {
 	};
 
 	return (
-		<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-			<div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
-				<h2 className="text-2xl font-bold mb-3 text-center">
-					Register
-				</h2>
-				<form onSubmit={handleSubmit(handleRegistration)}>
-					<div className="mb-4">
-						<div className="flex justify-center text-amber-500 h-5 mb-3">
-							<p>{errors.errors && errors.errors.message}</p>
+		<>
+		<head>
+			<title>Register - APUOPE-RE</title>
+		</head>
+			<div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+				<div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
+					<h2 className="text-2xl font-bold mb-3 text-center">
+						Register
+					</h2>
+					<form onSubmit={handleSubmit(handleRegistration)}>
+						<div className="mb-4">
+							<div className="flex justify-center text-amber-500 h-5 mb-3">
+								<p>{errors.errors && errors.errors.message}</p>
+							</div>
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="email"
+							>
+								Username
+							</label>
+							<input
+								id="username"
+								type="username"
+								placeholder="Enter your name"
+								className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+								{...register("username")}
+								required
+							/>
 						</div>
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="email"
-						>
-							Username
-						</label>
-						<input
-							id="username"
-							type="username"
-							placeholder="Enter your name"
-							className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-							{...register("username")}
-							required
-						/>
-					</div>
-					<div className="mb-4">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="email"
-						>
-							Email
-						</label>
-						<input
-							id="email"
-							type="email"
-							placeholder="Enter your email"
-							className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-							{...register("email")}
-							required
-						/>
-					</div>
-					<div className="flex justify-center text-amber-500">
-						<p>
-							{errors.passwordFirst &&
-								errors.passwordFirst.message}
-						</p>
-					</div>
-					<div className="mb-6">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="password"
-						>
-							Password
-						</label>
-						<input
-							id="password"
-							type="password"
-							placeholder="Enter your password"
-							className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-							{...register("passwordFirst", {
-								validate: {
-									containsInvalidCharacters: value => !value || /^[a-zA-Z0-9@$!%*?&()#^]*$/.test(value) ||
-										"Password contains invalid characters. Only letters, numbers, and @$!%*?&()#^ are allowed.",
-									minLength: value => !value || value.length >= 8 || "Password must be at least 8 characters long",
-									maxLength: value => !value || value.length <= 20 || "Password cannot exceed 20 characters",
-									containLowercase: value => !value || /[a-z]/.test(value) || "Password must include at least one lowercase letter",
-									containsUppercase: value => !value || /[A-Z]/.test(value) || "Password must include at least one uppercase letter",
-									containsNumber: value => !value || /[0-9]/.test(value) || "Password must include at least one number",
-									containsSpecial: value => !value || /[@$!%*?&()#^]/.test(value) || "Password must include at least one special character",
-								}
-							})}
-							required
-						/>
-					</div>
-					<div className="mb-6">
-						<label
-							className="block text-gray-700 text-sm font-bold mb-2"
-							htmlFor="password"
-						>
-							Confirm Password
-						</label>
-						<input
-							id="confirmPassword"
-							type="password"
-							placeholder="Re-enter your password"
-							className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-							{...register("passwordSecond")}
-							required
-						/>
-					</div>
-					<div className="flex items-center justify-between">
-						<p className="">
-							{"Already have an account? "}
-							<span className="text-blue-800 font-semibold underline pl-1">
-								<Link href="/login">Login</Link>
-							</span>
-						</p>
-						<input
-							type="submit"
-							className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring"
-							value={"Register"}
-						/>
-					</div>
-				</form>
+						<div className="mb-4">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="email"
+							>
+								Email
+							</label>
+							<input
+								id="email"
+								type="email"
+								placeholder="Enter your email"
+								className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+								{...register("email")}
+								required
+							/>
+						</div>
+						<div className="flex justify-center text-amber-500">
+							<p>
+								{errors.passwordFirst &&
+									errors.passwordFirst.message}
+							</p>
+						</div>
+						<div className="mb-6">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="password"
+							>
+								Password
+							</label>
+							<input
+								id="password"
+								type="password"
+								placeholder="Enter your password"
+								className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+								{...register("passwordFirst", {
+									validate: {
+										containsInvalidCharacters: value => !value || /^[a-zA-Z0-9@$!%*?&()#^]*$/.test(value) ||
+											"Password contains invalid characters. Only letters, numbers, and @$!%*?&()#^ are allowed.",
+										minLength: value => !value || value.length >= 8 || "Password must be at least 8 characters long",
+										maxLength: value => !value || value.length <= 20 || "Password cannot exceed 20 characters",
+										containLowercase: value => !value || /[a-z]/.test(value) || "Password must include at least one lowercase letter",
+										containsUppercase: value => !value || /[A-Z]/.test(value) || "Password must include at least one uppercase letter",
+										containsNumber: value => !value || /[0-9]/.test(value) || "Password must include at least one number",
+										containsSpecial: value => !value || /[@$!%*?&()#^]/.test(value) || "Password must include at least one special character",
+									}
+								})}
+								required
+							/>
+						</div>
+						<div className="mb-6">
+							<label
+								className="block text-gray-700 text-sm font-bold mb-2"
+								htmlFor="password"
+							>
+								Confirm Password
+							</label>
+							<input
+								id="confirmPassword"
+								type="password"
+								placeholder="Re-enter your password"
+								className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+								{...register("passwordSecond")}
+								required
+							/>
+						</div>
+						<div className="flex items-center justify-between">
+							<p className="">
+								{"Already have an account? "}
+								<span className="text-blue-800 font-semibold underline pl-1">
+									<Link href="/login">Login</Link>
+								</span>
+							</p>
+							<input
+								type="submit"
+								className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring"
+								value={"Register"}
+							/>
+						</div>
+					</form>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
