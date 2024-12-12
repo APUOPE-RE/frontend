9f43a5cb32cfff4740aceaf2899d7762450ac99e
Emil Selroos <41218215+emilselroos@users.noreply.github.com>
2024-12-12 22:47:24 +0200

    Add chat options (#111)
    * Remove preset container name to fix docker error
    
    * Add button options
    
    * Add modal dialog windows
    
    * Dump
    
    * Dump
    
    * Fetch conversations after changes and fix styling
    
    ---------
    
    Co-authored-by: roosa.vihlman <roosaa.vihlman@gmail.com>

--- src/app/chatbot/Conversations.tsx
+++ src/app/chatbot/Conversations.tsx
@@ -3,65 +3,68 @@ import { useAppContext } from "@/src/context";
 import { ChatBotRequestData, Message } from "../types/types";
 import { fetchConversation } from "../actions/chatbot";
 import { UseFormSetValue } from "react-hook-form";
+import ChatOptions from "@/src/components/ChatOptions";
 
 enum Source {
-	USER = 1,
-	BOT = 2,
+  USER = 1,
+  BOT = 2,
 }
 
 type ConversationsProps = {
-	currentConversation: number;
-	setMessages: Dispatch<SetStateAction<Message[]>>;
-	setValue: UseFormSetValue<ChatBotRequestData>;
+  currentConversation: number;
+  setMessages: Dispatch<SetStateAction<Message[]>>;
+  setValue: UseFormSetValue<ChatBotRequestData>;
 };
 
 export const Conversations = ({
-	currentConversation,
-	setMessages,
-	setValue,
+  currentConversation,
+  setMessages,
+  setValue,
 }: ConversationsProps) => {
-	const { conversations, materials } = useAppContext();
+  const { conversations, materials } = useAppContext();
 
-	const filterSubject = (id: number) => {
-		return materials.find((m) => m.id === id);
-	};
+  const filterSubject = (id: number) => {
+    return materials.find((m) => m.id === id);
+  };
 
-	const fetchMessages = async (conversationId: number) => {
-		if (conversationId !== currentConversation) {
-			setValue("conversationId", 0);
-			setValue("lectureId", 0);
-			const response = await fetchConversation(conversationId);
-			if (Array.isArray(response)) {
-				const messages: Message[] = response.map((m) => ({
-					from: m.source === Source.USER ? "user" : "bot",
-					message: m.content,
-				}));
-				setMessages(messages);
-			}
-		}
-	};
+  const fetchMessages = async (conversationId: number) => {
+    if (conversationId !== currentConversation) {
+      setValue("conversationId", conversationId);
+      setValue("lectureId", 0);
+      const response = await fetchConversation(conversationId);
+      if (Array.isArray(response)) {
+        const messages: Message[] = response.map((m) => ({
+          from: m.source === Source.USER ? "user" : "bot",
+          message: m.content,
+        }));
+        setMessages(messages);
+      }
+    }
+  };
 
-	return (
-		<>
-			{conversations.map((c) => {
-				return (
-					<button
-						key={c.id}
-						className="w-[99%] h-20 border p-2 border-none rounded-lg mb-2 bg-gray-100 shadow-md hover:bg-blue-100"
-						onClick={() => fetchMessages(c.id)}
-					>
-						<p className="text-xs text-right">
-							{new Date(c.dateTime).toLocaleDateString()}
-						</p>
-						<p className="pt-3 pl-3 text-left">
-							{
-								filterSubject(c.chapterId > 0 ? c.chapterId : 1)
-									?.label
-							}
-						</p>
-					</button>
-				);
-			})}
-		</>
-	);
+  return (
+    <>
+      {conversations.map((c) => {
+        return (
+          <div
+            key={c.id}
+            className={`relative w-[99%] h-20 border p-2 border-none rounded-lg mb-2 shadow-md hover:bg-blue-100 cursor-pointer ${
+              currentConversation === c.id ? "bg-blue-100" : "bg-gray-100"
+            }`}
+            onClick={() => fetchMessages(c.id)}
+          >
+            <span className="absolute top-2 right-2 text-xs">
+              {new Date(c.dateTime).toLocaleDateString()}
+            </span>
+            <div className="h-full flex items-center justify-between">
+              <p>{c.subject}</p>
+              {currentConversation === c.id && (
+                <ChatOptions conversation={c} setValue={setValue} />
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
 };
